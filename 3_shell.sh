6df143895112b60ec2a1985402e8e9498d8197aa
DuckLL <a347liao@gmail.com>
2018-04-15 14:28:15 +0800

    tpm used

--- 3_shell.sh
+++ 3_shell.sh
@@ -22,7 +22,9 @@ cp .gitconfig ~/
 cp .gitignore_global ~/
 
 # set vim
-pip2 install neovim
-pip3 install neovim
+pip install neovim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +q +UpdateRemotePlugins +q
+
+# set tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
