87309138aa35a42f2362b01554bc1b0a459f0657
DuckLL <a347liao@gmail.com>
2019-04-23 23:32:01 +0800

    remove py2

--- linux.sh
+++ linux.sh
@@ -12,8 +12,6 @@ cp .gitconfig ~/
 cp .gitignore_global ~/
 
 # set vim
-pip2 install pip -U
-pip2 install neovim --user
 pip3 install pip -U
 pip3 install neovim --user
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
