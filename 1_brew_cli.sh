26baf4ab9cec44ed536c557f19438ce99d3d1722
DuckLL <a347liao@gmail.com>
2019-11-09 22:50:37 +0800

    fix tmux bug

--- 1_brew_cli.sh
+++ 1_brew_cli.sh
@@ -9,6 +9,7 @@ brew  install  dex2jar
 brew  install  ffmpeg
 brew  install  git
 brew  install  httrack
+brew  install  hugo
 brew  install  mas
 brew  install  mycli
 brew  install  neovim
@@ -17,6 +18,7 @@ brew  install  p7zip
 brew  install  python
 brew  install  reattach-to-user-namespace
 brew  install  rmtrash
+brew  install  scrcpy
 brew  install  tmux
 brew  install  yarn
 brew  install  zsh
--- 3_shell.sh
+++ 3_shell.sh
@@ -22,7 +22,7 @@ cp .gitconfig ~/
 cp .gitignore_global ~/
 
 # set vim
-pip install neovim
+pip3 install neovim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +q +UpdateRemotePlugins +q
 
