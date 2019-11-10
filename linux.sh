b00bfd4fa9f341deefe5d9884fd119003c581e84
DuckLL <a347liao@gmail.com>
2019-11-10 11:30:18 +0800

    remove set vim in linux

--- linux.sh
+++ linux.sh
@@ -11,8 +11,3 @@ cp .gemrc ~/
 cp .gitconfig ~/
 cp .gitignore_global ~/
 
-# set vim
-pip3 install pip -U
-pip3 install pynvim --user
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim +PlugInstall +q +UpdateRemotePlugins +q
