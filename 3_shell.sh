586b275230bdfec78674952693bd0f0542994031
DuckLL <a347liao@gmail.com>
2022-08-14 14:32:18 +0800

    build-in no support true color

--- 3_shell.sh
+++ 3_shell.sh
@@ -26,7 +26,7 @@ pip3 install --user \
 # vim
 cp .vimrc ~/
 mkdir -p ~/.config/nvim
-ln ~/.vimrc ~/.config/nvim/init.vim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
 pip3 install neovim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +q +UpdateRemotePlugins +q
